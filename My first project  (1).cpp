721e6c33be5b76de415f80090ce7fad9aa3f4b96
vveerayt0-collab <vveerayt0@gmail.com>
2025-09-16 13:38:37 +0530

    Add files via upload

---
+++ My first project  (1).cpp
@@ -0,0 +1,25 @@
+<textarea name="message" placeholder="Your Message" rows="5" required></textarea>
+
+        <button type="submit" class="btn">Send Message</button>
+
+        <p id="formStatus"></p>
+
+      </form>
+
+    </div>
+
+  </section>
+
+  <!-- Footer -->
+
+  <footer class="footer">
+
+    <p>© <span id="year"></span>subhasri  | Made with 💖</p>
+
+  </footer>
+
+  <script src="script.js"></script>
+
+</body>
+
+</html>
